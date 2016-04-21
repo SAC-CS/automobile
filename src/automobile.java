d3077856755fd70a7357e5d86cff094127cf7b7c
MiguelBasilio <lokoa@DESKTOP-F4BVPJT>
2016-04-20 22:00:42 -0700

    constructor

---
+++ src/automobile.java
@@ -0,0 +1,53 @@
+
+public class automobile {
+	
+	private int NumbDoors;
+	
+	public void setNumberDoors (){
+		this.NumbDoors = NumbDoors;
+	}
+	private int NumbCilinders;
+	
+	public void setCilinders(){
+		this.NumbCilinders = NumbCilinders;
+	}
+	private String Transmission;
+	
+	public void setTransmission(){
+		this.Transmission = Transmission;
+	}
+	private String Color;
+	
+	public void setColor(){
+		this.Color = Color;	
+	}
+	private String Name;
+	public void setName(){
+	
+	this.Name = "Cabbie";
+	}
+	
+	public int getNumberDoors()
+	{
+		return NumbDoors;
+	}
+	public int getCilinders(){
+		return NumbCilinders;
+	}
+	public String getTransmission(){
+		return Transmission;
+	}
+	public String getColor(){
+		return Color;
+	}
+	public String getName(){
+		return Name;
+		
+	}
+
+
+
+
+
+}
+
